decc03327e5d00df5c285d8449276ea0f595c35e
Momina Tufail <momnatufail30@gmail.com>
2025-01-15 20:33:08 +0500

    code updated

--- lib/gold-home.dart
+++ lib/gold-home.dart
@@ -188,8 +188,7 @@ class _GoldHomeState extends State<GoldHome> {
               backgroundColor: Colors.white.withOpacity(1),
 
             );
-           }
-           ),
+           }),
         ],
         ),
       ),
